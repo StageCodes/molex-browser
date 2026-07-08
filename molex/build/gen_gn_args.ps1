b039fa4f9bb99f05ce2bd1ef79a4ef4cc16da151
Molex <molex@localhost>
2026-07-09 00:08:55 +0500

    Disable PGO (chrome_pgo_phase=0), free disk space, add --no-history to Android

--- molex/build/gen_gn_args.ps1
+++ molex/build/gen_gn_args.ps1
@@ -5,6 +5,7 @@ $lines = @(
   "is_debug = false",
   "symbol_level = 0",
   "proprietary_codecs = true",
+  "chrome_pgo_phase = 0",
   'ffmpeg_branding = "Chrome"',
   "enable_widevine = true",
   'branding = "molex"'
