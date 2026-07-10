8964d09b3dc7dc51404a0c55a0091946c82b55c4
Molex <molex@localhost>
2026-07-10 12:58:27 +0500

    Add ccache with GitHub cache (survives timeout, saves partial build)

--- molex/build/gen_gn_args.ps1
+++ molex/build/gen_gn_args.ps1
@@ -6,6 +6,7 @@ $lines = @(
   "symbol_level = 0",
   "blink_symbol_level = 0",
   "optimize_for_size = true",
+  "cc_wrapper = \"ccache\"",
   "proprietary_codecs = true",
   "chrome_pgo_phase = 0",
   'ffmpeg_branding = "Chrome"',
